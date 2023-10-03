aae107061fc35fb56a0708d6d940e5a1ca30aa4e
sefufim <132181028+sefufim@users.noreply.github.com>
2023-10-02 22:19:11 -0700

    added tests and centered the svg text within the svg

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const {Circle, Rectangle, Triangle} = require("./lib/shapes");
 // generates SVG from the user data
 function generateSVG(shapeChoice, { letters, textColor, shapeColor }) {
     
-    const shapeRepresentation = shapeChoice.render();
-    const textElement = `<text x="10" y="20" fill="${textColor}">${letters}</text>`;
+    const chosenShape = shapeChoice.render();
+    const textElement = `<text x="150" y="100" dominant-baseline="middle"  stroke-width="0" stroke="#000" font-size="24px" text-anchor="middle" fill="${textColor}">${letters}</text>`;
   
-    const svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200" fill = ${shapeColor}>${shapeRepresentation}${textElement}</svg>`;
+    const svg = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200" fill = ${shapeColor}>${chosenShape}${textElement}</svg>`;
     return svg;
   }
 
@@ -53,32 +53,35 @@ function svgFile (fileName , data) {
 
 
 function init() {
-    inquirer
+  inquirer
     .prompt(questions)
     .then((answers) => {
+      // checks to see if user went over 3 letters
+      if (answers.letters.length < 0 || answers.letters.length > 3) {
+        console.log("Please do not exceed 3 letters");
+      } else {
+        let shapeChoice;
+        // switch case to determine what shape the user wants
+        switch (answers.logo) {
+          case 'Triangle':
+            shapeChoice = new Triangle();
+            break;
+          case 'Rectangle':
+            shapeChoice = new Rectangle();
+            break;
+          case 'Circle':
+            shapeChoice = new Circle();
+            break;
+          default:
+            console.error('Invalid shape');
+            return;
+        }
+        const svgData = generateSVG(shapeChoice, answers);
+        svgFile("logo.svg", svgData);
+      }
+    });
+}
 
-    let shapeChoice;
-// switch case to determine what shape the user wants
-    switch (answers.logo) {
-      case 'Triangle':
-        user_shapence = new Triangle();
-        break;
-      case 'Rectangle':
-        shapeChoice = new Rectangle();
-        break;
-      case 'Circle':
-        shapeChoice = new Circle();
-        break;
-      default:
-        console.error('Invalid shape');
-        return;
-    }
-    const svgData = generateSVG(shapeChoice, answers);
-    svgFile("logo.svg", svgData);
-    })
-    
-
-  };
 
   // calling the function
   init();
